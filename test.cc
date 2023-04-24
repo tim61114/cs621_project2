fc79990cabd28f7f6e3944df029baa5513b16719
project <tim61114@gmail.com>
2023-04-24 21:04:27 +0000

    Added source-ip-filter-element.cc, renamed Filter Element

--- test.cc
+++ test.cc
@@ -1,7 +1,8 @@
 #include <cstdio>
 // #include "ns3/core-module.h"
 // #include "ns3/network-module.h"
-#include "FilterElement.cc"
+//#include "filter-element.cc"
+#include "source-ip-filter-element.cc"
 
 using namespace ns3;
 
@@ -18,5 +19,10 @@ int main() {
     test t1;
     Ptr<Packet> pkt = Create<Packet>(101);
     t1.match(pkt);  
+    
+    Ipv4Address address("102.102.102.102");
+    SourceIPAddressFilter s1(address);
+    Ptr<Packet> pkt2 = Create<Packet>(101);
+    printf("%d\n", s1.match(pkt2));
 }
 
