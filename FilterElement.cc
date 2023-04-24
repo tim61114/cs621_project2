d6470ab39f113d44bc9ecfe3355d2868d3969eaf
xiuhuiwang <xiuhuiwang91@gmail.com>
2023-04-24 13:06:26 -0700

    test finished

--- FilterElement.cc
+++ FilterElement.cc
@@ -1,12 +1,12 @@
-// #include "ns3/packet.h"
-// #include "ns3/ptr.h"
+#include "ns3/packet.h"
+#include "ns3/ptr.h"
 
 namespace ns3{
 
 class FilterElement {
     public: 
-        //virtual bool match(Ptr<ns3::Packet>) = 0;
-        virtual bool match(int a) = 0;
+        virtual bool match(Ptr<ns3::Packet> myPackets) = 0;
+        //virtual bool match(int a) = 0;
 };
 
 }
--- test.cc
+++ test.cc
@@ -1,17 +1,22 @@
 #include <cstdio>
+// #include "ns3/core-module.h"
+// #include "ns3/network-module.h"
 #include "FilterElement.cc"
 
+using namespace ns3;
 
 class test : ns3::FilterElement {
+    
     public:
-        bool match(int a) override {
-            printf("Hello test\n");
+        bool match(Ptr<ns3::Packet> myPackets) override {
+            printf("%d\n", myPackets->GetSize());
             return true;
         }
 };
 
 int main() {
     test t1;
-    t1.match(1);  
+    Ptr<Packet> pkt = Create<Packet>(101);
+    t1.match(pkt);  
 }
 
