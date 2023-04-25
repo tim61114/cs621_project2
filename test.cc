60fa0c539361945ce116b8c6f7b6a4f9f82b8b82
project <tim61114@gmail.com>
2023-04-25 06:39:13 +0000

    Added dest-mask-filter-element.cc, source-mask-filter-element.cc, added tests for source-mask as proof of concept.

--- test.cc
+++ test.cc
@@ -3,6 +3,7 @@
 // #include "ns3/network-module.h"
 //#include "filter-element.cc"
 #include "source-ip-filter-element.cc"
+#include "source-mask-filter-element.cc"
 
 using namespace ns3;
 
@@ -20,9 +21,16 @@ int main() {
     Ptr<Packet> pkt = Create<Packet>(101);
     t1.match(pkt);  
     
-    Ipv4Address address("102.102.102.102");
-    SourceIPAddressFilter s1(address);
+    Ipv4Address address1("102.102.102.102");
+    SourceIPAddressFilter s1(address1);
     Ptr<Packet> pkt2 = Create<Packet>(101);
     printf("%d\n", s1.match(pkt2));
+
+    Ipv4Address address2("102.102.0.1");
+    Ipv4Mask mask1("255.255.0.0");
+    SourceMaskFilter s2(address1, mask1);
+    printf("%d\n", s2.match(pkt2));
+    
+
 }
 
