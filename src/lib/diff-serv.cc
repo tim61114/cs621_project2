e18a0f4baae282b880669823e75c2123365759b6
project <tim61114@gmail.com>
2023-04-28 21:04:40 +0000

    Modified diff-serv.cc

--- src/lib/diff-serv.cc
+++ src/lib/diff-serv.cc
@@ -1,47 +1,45 @@
-//#include <vector>
-//#include <cstdint>
-//#include "traffic-class.cc"
-//#include "filter.cc"
-//#include "ns3/queue.h"
-//
-//using namespace ns3;
-//
-//class DiffServ : Queue<Packet> {
-//    private:
-//        QueueMode m_mode;
-//        std::vector<TrafficClass> q_class;
-//
-//    public:
-//        bool DoEnqueue(Ptr<Packet> p) override {
-//
-//        }
-//
-//        Ptr<Packet> DoDequeue() override {
-//
-//        }
-//
-//        Ptr<Packet> DoRemove() override {
-//
-//        }
-//
-//        const Ptr<Packet> DoPeek() override {
-//
-//        }
-//
-//        void SetMode(QueueMode mode) override {
-//
-//        }
-//
-//        QueueMode GetMode() override {
-//            
-//        }
-//
-//        Ptr<Packet> Schedule() override {
-//
-//        }
-//
-//        uint32_t Classify(Ptr<Packet> p) override {
-//            
-//        }
-//
-//};
+#include <vector>
+#include <cstdint>
+#include "traffic-class.cc"
+#include "filter.cc"
+#include "ns3/queue.h"
+
+using namespace ns3;
+
+class DiffServ : Queue<Packet> {
+    private:
+        std::vector<TrafficClass> q_class;
+
+    public:
+        bool DoEnqueue(Ptr<Packet> p) override {
+            for (auto& tc : q_class) {
+                if (!tc.match(p)) {
+                    continue;
+                }
+                return tc.Enqueue(p);
+            }
+            // Packet dropped.
+            return false;
+        }
+
+        Ptr<Packet> DoDequeue() override {
+            
+        }
+
+        Ptr<Packet> DoRemove() override {
+
+        }
+
+        const Ptr<Packet> DoPeek() override {
+
+        }
+
+        Ptr<Packet> Schedule() override {
+
+        }
+
+        uint32_t Classify(Ptr<Packet> p) override {
+            
+        }
+
+};
