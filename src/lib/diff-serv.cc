875a801fc240e17d63285e8f50d02b2279f58ae5
project <tim61114@gmail.com>
2023-04-30 06:05:41 +0000

    Added diff-serv.cc

--- src/lib/diff-serv.cc
+++ src/lib/diff-serv.cc
@@ -7,13 +7,13 @@
 using namespace ns3;
 
 class DiffServ : Queue<Packet> {
-    private:
+    protected:
         std::vector<TrafficClass> q_class;
 
     public:
         bool DoEnqueue(Ptr<Packet> p) override {
             for (auto& tc : q_class) {
-                if (!tc.match(p)) {
+                if (!tc.match(p) || tc.isFull()) {
                     continue;
                 }
                 return tc.Enqueue(p);
@@ -22,24 +22,18 @@ class DiffServ : Queue<Packet> {
             return false;
         }
 
-        Ptr<Packet> DoDequeue() override {
-            
+        Ptr<Packet> Schedule() {
+            return DoDequeue();
         }
 
-        Ptr<Packet> DoRemove() override {
+        uint32_t Classify(Ptr<Packet> p) {
+            for (auto& tc : q_class) {
+                if (tc.match(p) && !tc.isFull()) {
+                    return tc.getPriorityLevel();
+                }
+            }
 
-        }
-
-        const Ptr<Packet> DoPeek() override {
-
-        }
-
-        Ptr<Packet> Schedule() override {
-
-        }
-
-        uint32_t Classify(Ptr<Packet> p) override {
-            
+            return -1;
         }
 
 };
